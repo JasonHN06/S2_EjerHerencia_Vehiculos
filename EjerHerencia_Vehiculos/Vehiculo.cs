c9415c06e35f0c5bbf5e30bc9fd99bda2ee43874
Jason Madrid <jasonhnn6@gmail.com>
2025-08-09 17:12:13 -0600

    create superclass

---
+++ EjerHerencia_Vehiculos/Vehiculo.cs
@@ -0,0 +1,15 @@
+﻿namespace EjerHerencia_Vehiculos
+{
+    internal class Vehiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anno { get; set; }
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"Marca: {Marca}");
+            Console.WriteLine($"Modelo: {Modelo}");
+            Console.WriteLine($"Año: {Anno}");
+        }
+    }
+}
